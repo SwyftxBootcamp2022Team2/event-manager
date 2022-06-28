095963f7a57bf4cc3e7619d3133450c691d5eaa1
Nathan Hettige <nathan.hettige@gmail.com>
2022-06-28 16:32:47 +1000

    Added formik and yup

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import {
+  FormControl, FormLabel, Input,
+} from '@chakra-ui/react';
+
+const loginSchema = Yup.object({
+  email: Yup.string().email().required(),
+});
 
 function LoginPage() {
-  return <div>This is the login page</div>;
+  return (
+    <Formik initialValues={{ email: '' }} validationSchema={loginSchema} onSubmit={() => console.log('submit')}>
+      <form>
+        <FormControl>
+          <FormLabel htmlFor="email">Email address</FormLabel>
+          <Input id="email" type="email" />
+        </FormControl>
+      </form>
+    </Formik>
+  );
 }
 
 export default LoginPage;
