d4ea95989cad576740082d4629fa9f26b8ed43e9
HimathR <himath4510@gmail.com>
2022-06-28 22:27:23 +1000

    added simple api routes

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask
 import sqlite3 as sql
+import app 
+import requests
 
 @app.route("/")
 def hello():
@@ -11,4 +13,26 @@ def login():
     email = requests.form['email']
     con.close()
 
-    
+@app.route("/home", methods=['GET'])
+def home_function():
+    return True
+
+@app.route("/event/create", methods=['POST']) # admin is the one who creates
+def create_event():
+    return True
+
+@app.route("/event/delete", methods=['DELETE'])
+def delete_event():
+    return True
+
+@app.route("/event/view", methods=['GET'])
+def view_event():
+    return True
+
+@app.route("/event/book", methods=['POST']) # user is the one who books
+def book_event():
+    return True
+
+@app.route("/event/unbook", methods=['DELETE']) # user is the one who books
+def unbook_event():
+    return True
