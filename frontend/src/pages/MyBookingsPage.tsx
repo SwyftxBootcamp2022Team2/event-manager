5071ac888e6c9bb622bcd887a5c822aa1cd2673e
Nathan Hettige <nathan.hettige@gmail.com>
2022-06-29 17:01:02 +1000

    Added a navigation bar.

---
+++ frontend/src/pages/MyBookingsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MyBookingsPage() {
+  return <div>This is my bookings page</div>;
+}
+
+export default MyBookingsPage;
---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProfilePage() {
+  return <div>This is the profile page</div>;
+}
+
+export default ProfilePage;
