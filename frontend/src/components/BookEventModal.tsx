06a4c606a77f5e12d876c8353417bf8472e63b5c
Nathan Hettige <nathan.hettige@gmail.com>
2022-07-01 15:09:58 +1000

    Render picture in event modal

--- frontend/src/components/BookEventModal.tsx
+++ frontend/src/components/BookEventModal.tsx
@@ -9,14 +9,23 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Text,
+  Image,
+  Heading,
+  HStack,
+  Center,
 } from '@chakra-ui/react';
 import { useNavigate, useParams } from 'react-router-dom';
+import PancakePhoto from '../assets/pancake.jpeg';
 
 function BookEventModal() {
   const { id } = useParams();
   const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const data = { title: 'Pancake Tuesday' };
+  const data = {
+    title: 'Pancake Tuesday',
+    image: PancakePhoto,
+  };
 
   useEffect(() => {
     onOpen();
@@ -29,12 +38,32 @@ function BookEventModal() {
         onClose();
         navigate(-1);
       }}
+      size="4xl"
     >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>{data.title}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>Hello World {id}</ModalBody>
+      <ModalContent maxH="85%">
+        <ModalHeader paddingBottom="25px" paddingTop="40px">
+          <Heading>Pancake Tuesday</Heading>
+        </ModalHeader>
+
+        <ModalCloseButton size="lg" m="25px" />
+
+        <ModalBody>
+          <HStack>
+            <Image
+              alignContent="center"
+              boxSize="sm"
+              src={data.image}
+              alt="Dan Abramov"
+              fit="cover"
+              borderRadius={5}
+            />
+            <Center h="100%" w="100%">
+              <Text>Hello World {id}</Text>
+            </Center>
+          </HStack>
+        </ModalBody>
+
         <ModalFooter>
           <Button
             bg="#0072ed"
