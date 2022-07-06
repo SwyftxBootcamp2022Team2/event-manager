587ae2f2e2d36333b813fe9aa50e734e20bdaf6c
Nathan Hettige <nathan.hettige@gmail.com>
2022-07-07 00:42:53 +1000

    Added buttons to reporting page

--- frontend/src/pages/ReportingPage.tsx
+++ frontend/src/pages/ReportingPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Box, Heading, Link, Text } from '@chakra-ui/react';
 import Events from '../api/EventsEntity';
 import Bookings from '../api/BookingsEntity';
 import EventStats from '../components/EventStats';
@@ -17,7 +18,34 @@ function ReportingPage() {
     }
   }, [user]);
 
-  return <EventStats eventCount={eventsData.length} rsvpCount={rsvpCount} />;
+  const reportingOptions = [
+    { title: 'Export CSV' },
+    { title: 'Export something else' },
+  ];
+
+  return (
+    <>
+      <div style={{ marginLeft: 75 }}>
+        <Heading size="2xl" paddingBottom="25px" paddingTop="40px">
+          Reporting
+        </Heading>
+        <Box pl={5} borderRadius={5} w="70%" bg="#FFFEFE">
+          <Box pl={5} borderRadius={5} w="70%" bg="#FFFEFE">
+            {reportingOptions.map((option) => (
+              <Box p={4} key={option.title}>
+                <Text fontSize="3xl" paddingBottom={2}>
+                  <Link href="http://localhost:3000/reporting">
+                    {option.title}
+                  </Link>
+                </Text>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </div>
+      <EventStats eventCount={eventsData.length} rsvpCount={rsvpCount} />
+    </>
+  );
 }
 
 export default ReportingPage;
